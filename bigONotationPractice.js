9d39d5abca07bbe0187153333d8033c290c5a792
Marcus Perry <marcus.perry2017@gmail.com>
2023-03-29 20:10:57 -0400

    Add files via upload

---
+++ bigONotationPractice.js
@@ -0,0 +1,46 @@
+
+// Step One: Simplifying Expressions
+
+// 1.) O(n)
+// 2.) O(n)
+// 3.) O(1)
+// 4.) O(n^3)
+// 5.) O(n)
+// 6.) O(n)
+// 7.) O(n log n)
+// 8.) O(2^n)
+// 9.) O(1)
+// 10.) O(n^2)
+
+
+
+
+// Step Two: Calculating Time Complexity 
+
+// 1.) O(n)
+// 2.) O(n)
+// 3.) O(1)
+// 4.) O(n)
+// 5.) O(n^2)
+// 6.) O(n)
+
+
+
+
+
+// Step Three: short answer
+
+
+// 1.) True
+// 2.) True 
+// 3.) False
+// 4.) O(n)
+// 5.) O(n) 
+// 6.) O(n) Depinding on what the callback does
+// 7.) O(n log n)
+// 8.) O(n)
+// 9.) O(1)
+// 10.) O(1) in some cases it can be O(n)
+// 11.) O(n)
+// 12.) O(n)
+// 13.) O(n)
